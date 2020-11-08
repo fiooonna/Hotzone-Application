a47357b18f62f4c4c2160c04d205e1eef20f4a41
Johnny Fung <yatpanfungj@gmail.com>
2020-11-09 00:55:29 +0800

    Add django depandencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { createUseStyles } from "react-jss"
-import HomePage from "./components/Views/HomePage"
-import Login from "./components/Views/Login"
+import HomePage from "components/Views/HomePage"
+import Login from "components/Views/Login"
 
 function App() {
   return (

4d32bf3eea60ba4fd14305c13fddeb6aaa8abc0e
Johnny Fung <yatpanfungj@gmail.com>
2020-11-21 20:05:16 +0800

    Add debug

--- hotzone/settings.py
+++ hotzone/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =  env('HOTZONE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env.bool('HOTZONE_DEBUG', default=False)
 
 ALLOWED_HOSTS = ['hotzone-groupv.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0']
 
